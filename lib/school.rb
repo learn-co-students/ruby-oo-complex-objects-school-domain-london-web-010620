8a705bc890b09e0d1c67600aed106c513384eec8
github username <github email address>
2020-01-07 23:32:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,9 @@
 # code here!
+class School
+    school = Schoo.new("Bayside High School")
+    school.roster
+    school.add_student("Zach morris", 9)
+    school.roster
+    school.grade(9)
+    school.sort
+    
